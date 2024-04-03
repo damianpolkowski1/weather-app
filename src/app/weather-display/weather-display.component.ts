abfe8edea426dc94f4710a8c8906637885aa733a
Damian <damianpolkowski13@gmail.com>
2024-04-03 09:44:43 +0200

    Fixed project settings

--- src/app/weather-display/weather-display.component.ts
+++ src/app/weather-display/weather-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TimeData, WeatherData } from '../weather-data';
 
 @Component({
